4a6d79b765151480a358648e6ce6d5cd2fd2096f
Asseldonk <m.d.a.v.asseldonk@student.tue.nl>
2018-03-23 11:32:20 +0100

    Initial Commit

---
+++ eclipse-workspace/loan-approval/src/main/java/org/camunda/bpm/getstarted/loanapproval/ProcessRequestDelegate.java
@@ -0,0 +1,15 @@
+package org.camunda.bpm.getstarted.loanapproval;
+
+import java.util.logging.Logger;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class ProcessRequestDelegate implements JavaDelegate {
+
+  private final static Logger LOGGER = Logger.getLogger("LOAN-REQUESTS");
+
+  public void execute(DelegateExecution execution) throws Exception {
+    LOGGER.info("Processing request by '" + execution.getVariable("customerId") + "'...");
+  }
+
+}
